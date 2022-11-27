f8b828859f47bd0fd66583069b4bc217fbcea1e8
Gabriel Bello <gabego@yahoo.com>
2022-11-26 22:34:34 -0500

    Ver.1.0

---
+++ src/models/users.models.js
@@ -0,0 +1,53 @@
+//* {
+//*    id: 1,
+//*    first_name: 'string',
+//*    last_name: 'string',
+//*    email: 'string',
+//*    password: 'string',
+//*    birthday: 'YYYY/MM/DD'
+//* }
+
+
+const { DataTypes } = require('sequelize')
+
+const db = require('../utils/database')
+
+const Users = db.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATEONLY
+    }
+}, {
+    timestamps: false //? No agrega las columnas created_at y updated_at
+}) 
+
+
+module.exports = Users
+
+
+
+
